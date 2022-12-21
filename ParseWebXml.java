834b465175247162fbfa997cf6838b9d0cedda53
tsvetomiraD <95402102+tsvetomiraD@users.noreply.github.com>
2022-12-21 09:07:27 +0200

    Update ParseWebXml.java

--- ParseWebXml.java
+++ ParseWebXml.java
@@ -1,6 +1,7 @@
-package WarInfo;
+package Parsers;
 
 import Servlet.HttpServlet;
+import Servlet.ServletInfo;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -9,20 +10,20 @@ import org.w3c.dom.NodeList;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.File;
-import java.util.HashMap;
-import java.util.Map;
 
 public class ParseWebXml {
-    static Map<String, ServletWarInfo> serMap = new HashMap<>();
-    public Map<String, ServletWarInfo> parse() throws Exception{
+    static ServletInfo servletInfo = new ServletInfo();
+    public ServletInfo parse(String path, String docBase) throws Exception{
         File file = new File("src/web.xml");
         DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
         DocumentBuilder db = dbf.newDocumentBuilder();
         Document doc = db.parse(file);
         doc.getDocumentElement().normalize();
+        servletInfo.docBase = docBase;
+        servletInfo.path = path;
         getServlet(doc, "servlet", "servlet-name", "servlet-class");
-        getServletMappers(doc, "servlet-mapping", "servlet-name", "url-pattern");
-        return serMap;
+        getServletMappers(doc, "servlet-mapping", "servlet-name", "url-pattern", path);
+        return servletInfo;
     }
 
     private static void getServlet(Document doc, String tagName, String name, String sClass) throws ClassNotFoundException {
@@ -32,19 +33,18 @@ public class ParseWebXml {
             Element eElement = (Element) node;
             String serName = eElement.getElementsByTagName(name).item(0).getTextContent();
             Class<? extends HttpServlet> serClass = (Class<? extends HttpServlet>) Class.forName(eElement.getElementsByTagName(sClass).item(0).getTextContent());
-            serMap.put(serName, new ServletWarInfo(serClass, serName));
+            servletInfo.addServlet(serName, serClass);
         }
     }
 
-    private static void getServletMappers(Document doc, String tagName, String name, String urlInfo) {
+    private static void getServletMappers(Document doc, String tagName, String name, String urlInfo, String path) {
         NodeList servlet = doc.getElementsByTagName(tagName);
         for (int i = 0; i < servlet.getLength(); i++) {
             Node node = servlet.item(i);
             Element eElement = (Element) node;
             String serName = eElement.getElementsByTagName(name).item(0).getTextContent();
-            String url = eElement.getElementsByTagName(urlInfo).item(0).getTextContent();
-            ServletWarInfo s = serMap.get(serName);
-            s.url = url;
+            String url = path + eElement.getElementsByTagName(urlInfo).item(0).getTextContent();
+            servletInfo.addServletPath(serName, url);
         }
     }
 }

1f300ba2b28b57993017378640e2b895c12f5b38
tsvetomiraD <95402102+tsvetomiraD@users.noreply.github.com>
2022-12-23 10:10:33 +0200

    Update ServletContext.java

--- ServletContext.java
+++ ServletContext.java
@@ -1,104 +1,25 @@
 package Servlet;
 
 import java.io.*;
-import java.util.Arrays;
-import java.util.Date;
+import java.util.*;
 
-public class ServletContext extends HttpServlet {
-    public String docBase;
-    public String path;
-    
-    public ServletContext(String baseDir, String path) {
-        this.docBase = baseDir;
-        this.path = path;
+public class ServletContext  {
+    Map<String, RequestDispatcher> map = new HashMap<>();
+
+    public void addDispatcher(RequestDispatcher requestDispatcher, String path) {
+        map.put(path, requestDispatcher);
     }
-    
-    @Override
-    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        PrintWriter printWriter = resp.getWriter();
-        String searchFile = docBase + req.getPathInfo().substring(path.length());
-        File f = new File(searchFile);
-        if (!f.exists()) {
-            printWriter.write("Not found: 404");
-            printWriter.flush();
+
+    RequestDispatcher getRequestDispatcher(String path) {
+        return map.get(path);
+    }
+
+    public List<RequestDispatcher> getAllRequestDispatcher() {
+        List<RequestDispatcher> list = new ArrayList<>();
+        for (var m: map.entrySet()) {
+            list.add(m.getValue());
         }
-
-        if (f.isFile()) {
-            try {
-                writeResponseBody(f, printWriter);
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-            return;
-        }
-
-        if (checkForIndexHtml(f, printWriter))
-            return;
-
-
-        try {
-            setHtml(f, printWriter);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        return list;
     }
 
-    public void setHtml(File f, PrintWriter printWriter) throws Exception {
-        printWriter.write(Arrays.toString("<html>".getBytes("UTF-8")));
-
-        for (File fileEntry : f.listFiles()) {
-            String httpResponse = new Date() + "   " + "<a href='./" + fileEntry.getName() + "'>" + fileEntry.getName() + "</a>" + "</br>";
-            printWriter.write(Arrays.toString(httpResponse.getBytes("UTF-8")));
-        }
-        printWriter.write(Arrays.toString("</html>".getBytes("UTF-8")));
-    }
-
-    public boolean checkForIndexHtml(File f, PrintWriter printWriter) throws IOException {
-        File[] matches = f.listFiles((dir, name) -> name.startsWith("index") && name.endsWith(".html"));
-
-        if (matches.length != 0) {
-            BufferedReader reader = new BufferedReader(new FileReader(matches[0]));
-            String line = reader.readLine();
-            while (line != null) {
-                printWriter.println(line);
-                printWriter.flush();
-                line = reader.readLine();
-            }
-            reader.close();
-            printWriter.close();
-            return true;
-        }
-        return false;
-    }
-    private void writeResponseBody(File f, PrintWriter printWriter) throws Exception {
-        BufferedReader br = new BufferedReader(new FileReader(f));
-        String httpResponse = br.readLine();
-        while (httpResponse != null) {
-            printWriter.write(Arrays.toString(httpResponse.getBytes("UTF-8")));
-            printWriter.write(Arrays.toString("\n".getBytes("UTF-8")));
-            printWriter.flush();
-            httpResponse = br.readLine();
-        }
-    }
-
-    @Override
-    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        PrintWriter printWriter = resp.getWriter();
-        printWriter.write("Method not allowed");
-        printWriter.flush();
-    }
-
-    @Override
-    public void doDelete(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        PrintWriter printWriter = resp.getWriter();
-        printWriter.write("Method not allowed");
-        printWriter.flush();
-    }
-
-    @Override
-    public void doPut(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        PrintWriter printWriter = resp.getWriter();
-        printWriter.write("Method not allowed");
-        printWriter.flush();
-    }
 }

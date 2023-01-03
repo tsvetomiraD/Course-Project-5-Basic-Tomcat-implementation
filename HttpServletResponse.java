2a8bf2aafb1591e15f855deb9bcf455d093d2499
tsvetomiraD <95402102+tsvetomiraD@users.noreply.github.com>
2023-01-03 09:33:10 +0200

    Update HttpServletResponse.java

--- HttpServletResponse.java
+++ HttpServletResponse.java
@@ -3,13 +3,21 @@ package Servlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HttpServletResponse {
     Socket socket;
+    public List<Cookie> cookies;
     public HttpServletResponse(Socket socket) {
         this.socket = socket;
+        this.cookies = new ArrayList<>();
     }
     public PrintWriter getWriter() throws IOException {
         return new PrintWriter(socket.getOutputStream());
+    }
+
+    void addCookie(Cookie cookie) {
+        cookies.add(cookie);
     }
 }

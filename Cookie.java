e4a14c0e5d45ad99fd9e5ddc58fb9095b3693df9
tsvetomiraD <95402102+tsvetomiraD@users.noreply.github.com>
2023-01-03 09:32:19 +0200

    Add files via upload

---
+++ Cookie.java
@@ -0,0 +1,7 @@
+package Servlet;
+
+public class Cookie {
+    public void addCookie() {
+
+    }
+}
---
+++ HttpSession.java
@@ -0,0 +1,8 @@
+package Servlet;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HttpSession {
+    List<Cookie> cookieList = new ArrayList<>();
+}
